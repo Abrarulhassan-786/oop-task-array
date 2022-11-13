ccfd94fd6d9e6c39986e1a4cbebd7256b50c5195
Abrar ul Hassan <107148226+Abrarulhassan-786@users.noreply.github.com>
2022-11-13 10:55:34 +0500

    array concept C#
    QUESTION: Create a C# program that requests three names of people from the user and
     stores them in an array of objects of type Person. To do this, first create 
    a Person class that has a Name property of type string and override the ToString() method.

---
+++ OOP tASK 1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Oop_Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 3;
+            Person[] person = new Person[total];
+            for (int i = 0; i < total; i++)
+            {
+                person[i] = new Person()
+                {
+                    Name = Console.ReadLine()
+                }
+                ;
+
+            }
+            for (int i = 0; i < total; i++)
+            {
+                Console.WriteLine(person[i].ToString());
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+    public class Person
+    {
+        public string Name;
+        public new string ToString()
+        {
+            return Name;
+        }
+    }
+}
+    
+
